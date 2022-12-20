1b84f5eb27e88235d34b6ae07962c6929a453877
Amir Kakon <amir.kakon@mesh-systems.com>
2022-12-20 14:18:46 +0200

    end 7.99

--- server/client/src/components/Header.js
+++ server/client/src/components/Header.js
@@ -20,7 +20,7 @@ class Header extends Component {
         return (
             <nav>
             <div className="nav-wrapper">
-              <a className="left brand-logo">Email Survey</a>
+              <a className="left brand-logo" href="/">Email Survey</a>
               <ul className="right ">
                 {this.renderContent()}
               </ul>
