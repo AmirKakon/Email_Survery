602eec7d0c4169a24504a28bbaaf1b067c9a2ada
Amir Kakon <amir.kakon@mesh-systems.com>
2022-12-20 15:46:11 +0200

    end 8.110

--- server/client/src/components/Payments.js
+++ server/client/src/components/Payments.js
@@ -4,14 +4,19 @@ import StripeCheckout from 'react-stripe-checkout';
 class Payments extends Component {
     render()
     {
-        debugger;
         return (
             //defaults to usd and is in cents.
             <StripeCheckout
                 amount={500}
                 token={token => console.log(token)}
                 stripeKey={process.env.REACT_APP_STRIPE_KEY}
-            />
+                name='Email Survey'
+                description='$5 for 5 email credits.'
+            >
+                <button className='btn'>
+                   Add Credits 
+                </button>
+            </StripeCheckout>
         );
     }
 }
