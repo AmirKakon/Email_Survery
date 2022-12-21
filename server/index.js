113a5922f7bdec74147cd16b0536a653c7f62d81
Amir Kakon <amir.kakon@mesh-systems.com>
2022-12-21 11:36:06 +0200

    end 9.124

--- server/index.js
+++ server/index.js
@@ -26,6 +26,18 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if(process.env.NODE_ENV === 'production')
+{
+    //we will look into the production assets located in client/build/static/js/main.js file
+    app.use(express.static('client/build'));
+
+    //if we dont understand the route (does not exist in production assets) we will handle it on the react client side.
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 // default port is 5000 (for development purposes)
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
