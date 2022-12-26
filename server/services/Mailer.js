e4014a9d8a356efbeaefd8a2bb62e8210bc39505
Amir Kakon <amir.kakon@mesh-systems.com>
2022-12-26 12:28:03 +0200

    end 10.151

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -7,6 +7,8 @@ class Mailer extends helper.Mail {
     constructor({subject, recipients}, content) {
         super();
 
+        this.sgApi = sendgrid(keys.sendGridKey);
+
         this.from_email = new helper.Email('pppchevy@gmail.com');
         this.subject = subject;
         this.body = new helper.Content('text/html', content);
@@ -41,6 +43,18 @@ class Mailer extends helper.Mail {
             personalize.addTo(recipient);
         });
         this.addPersonalization(personalize);
+    }
+
+    async send() 
+    {
+        const request = this.sgApi.emptyRequest({
+            method: 'POST',
+            path: 'v3/mail/send',
+            body: this.toJSON()
+        });
+
+        const response = this.sgApi.API(request);
+        return response;
     }
 }
 
