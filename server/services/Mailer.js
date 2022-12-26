39a40423550ce1d412b3c70db51064fe7c1363b7
Amir Kakon <amir.kakon@mesh-systems.com>
2022-12-26 12:13:11 +0200

    end 10.148

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -3,7 +3,15 @@ const helper = sendgrid.mail;
 const keys = require('../config/keys');
 
 class Mailer extends helper.Mail {
+    //can use any object with subject and recipient and not specific to surveys
+    constructor({subject, recipients}, content) {
+        super();
 
+        this.from_email = new helper.Email('pppchevy@gmail.com');
+        this.subject = subject;
+        this.body = new helper.Content('text/html', content);
+        this.recipients = this.formatAddresses(recipients);
+    }
 }
 
 module.exports = Mailer;
