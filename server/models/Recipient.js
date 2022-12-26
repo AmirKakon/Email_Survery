e331cdf176e6d132ebab3c58e5a3c572630a81eb
Amir Kakon <amir.kakon@mesh-systems.com>
2022-12-26 11:00:51 +0200

    end 10.140

--- server/models/Recipient.js
+++ server/models/Recipient.js
@@ -3,7 +3,7 @@ const { Schema } = mongoose; // same as saying: const Schema = mongoose.Schema;
 
 const recipientSchema = new Schema(
    {
-    email: String,
+    email: {type: String, lowercase: true, trim: true},
     responded: {type: Boolean, default: false}
    }
 );
