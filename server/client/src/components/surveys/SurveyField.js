8203e2e76a6cd898e9d385a7ea0394dd93df13d3
Amir Kakon <amir.kakon@mesh-systems.com>
2022-12-27 11:34:57 +0200

    end 11.171

--- server/client/src/components/surveys/SurveyField.js
+++ server/client/src/components/surveys/SurveyField.js
@@ -1,10 +1,12 @@
 import React from "react";
 
-export default ({input, label}) => {  // {input} == props.input
+export default ({input, label, meta: {error, touched}}) => {  // {input} == props.input
     return (
+        // {touched && error}  == if input was touched and there is an error, print the error
         <div>
             <label>{label}</label>
             <input {...input} />
+            {touched && error}
         </div>
     );
 }
