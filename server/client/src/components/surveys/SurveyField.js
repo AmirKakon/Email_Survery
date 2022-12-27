93bbc24de5a78d1051e2943b0f7eed4ee0618dcf
Amir Kakon <amir.kakon@mesh-systems.com>
2022-12-27 11:42:59 +0200

    end 11.172

--- server/client/src/components/surveys/SurveyField.js
+++ server/client/src/components/surveys/SurveyField.js
@@ -5,8 +5,8 @@ export default ({input, label, meta: {error, touched}}) => {  // {input} == prop
         // {touched && error}  == if input was touched and there is an error, print the error
         <div>
             <label>{label}</label>
-            <input {...input} />
-            {touched && error}
+            <input {...input} style={{marginBottom: '5px'}}/>
+            <div className="red-text" style={{marginBottoem: '20px'}}>{touched && error}</div>
         </div>
     );
 }
