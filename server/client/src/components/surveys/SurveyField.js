a6331df76d369c4f33431e8cbaff7a8e28486a46
Amir Kakon <amir.kakon@mesh-systems.com>
2022-12-27 10:55:36 +0200

    end 11.165

---
+++ server/client/src/components/surveys/SurveyField.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import {reduxForm, Field} from 'redux-form';
+
+export default () => {
+    return (
+        <div>
+            SurveyField!
+        </div>
+    );
+}
