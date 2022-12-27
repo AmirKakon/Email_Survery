44b3e994fb0c066f25e76d3b29a3bfa1739a7ff4
Amir Kakon <amir.kakon@mesh-systems.com>
2022-12-27 13:35:17 +0200

    end 11.179

--- server/client/src/components/surveys/SurveyFormReview.js
+++ server/client/src/components/surveys/SurveyFormReview.js
@@ -1,10 +1,14 @@
 import React, { Component } from "react";
 import {connect} from 'react-redux';
 
-const SurveyFormReview = ({onCancel}) => {
+const SurveyFormReview = ({onCancel, formValues}) => {
     return (
         <div>
             <h5>Please confirm your entries</h5>
+            <div>
+                <label>Survey Title</label>
+                <div>{formValues.title}</div>
+            </div>
             <button className="yellow darken-3 btn-flat" onClick={onCancel}>
                 Back
             </button>
